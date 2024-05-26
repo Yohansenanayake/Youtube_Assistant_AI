b248408de03e7e1b82e171dd4d3e54c4ca6bd2c4
yohan_senanayake <e20363@eng.pdn.ac.lk>
2024-05-26 22:20:37 +0530

    Deleted unused code

--- test.py
+++
@@ -1,9 +0,0 @@
-import youtube_assistant as ya
-
-user_input = "What are the characteristics of life?"
-url = "https://youtu.be/tZE_fQFK8EY?si=cjPlr3k3nuYbo0xj"
-
-db = ya.create_vector_db_from_youtube(url)
-#cprint(db)
-response = ya.youtube_assistant(user_input,db)
-print(response)
